00b23fb729b911fc8973b487918e6a14ec41cd16
Pavlo Petrenko <agronom81@gmail.com>
2019-01-05 22:08:10 +0200

    initial app

---
+++ src/components/layout/Spinner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Spinner() {
+  return (
+    <div>
+      <div className="d-table mx-auto spinner-wrapper">
+        <i className="fas fa-spinner fa-pulse"></i>
+      </div>
+    </div>
+  )
+}
